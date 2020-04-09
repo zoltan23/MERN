91a05f1baf1cdce6ec5cf8cc24ba5c3fc5963e4f
Tim <tim@Tims-MacBook-Air.local>
2020-04-08 21:39:20 -0600

    edited card css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import Card from './Card'
+import Card from './Card';
+import './Home.css';
 
 
 export default function Home() {
 
     return (
         <div className="row">
-               <Card link = {'link1'} linkName = {'link'} />
-               <Card />
-               <Card />
+               <Card link = {'link1'} linkName = {'linkName1'} />
+               <Card link = {'link2'} linkName = {'linkName2'} />
+               <Card link = {'link3'} linkName = {'linkName3'} />      
+               <Card link = {'link3'} linkName = {'linkName3'} />      
+               <Card link = {'link3'} linkName = {'linkName3'} />      
+               <Card link = {'link3'} linkName = {'linkName3'} />      
+               <Card link = {'link3'} linkName = {'linkName3'} />      
         </div>
     )
 }
