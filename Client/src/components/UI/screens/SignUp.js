a1993439ae4167ae11d531a46d889c60eb9435f5
Tim <timothy_tassin@hotmail.com>
2020-06-18 17:05:12 -0600

    added spinner to signup

--- Client/src/components/UI/screens/SignUp.js
+++ Client/src/components/UI/screens/SignUp.js
@@ -6,7 +6,7 @@ import ErrorMessage from '../reusable-components/ErrorMessage.js';
 import Spinner from '../reusable-components/Spinner/Spinner.js';
 import './SignUp.css';
 
-export default function SignUp() {
+export default function SignUp(props) {
 
     const [errMsg, setErrMsg] = useState('')
     const [isDisabled, setIsDisabled] = useState(true)
@@ -23,7 +23,7 @@ export default function SignUp() {
     useEffect(() => {
         const isUser = Object.values(userInfo).every(el => Boolean(el))
         const { password, confirmPassword } = userInfo
-        isUser && (password ===confirmPassword) ? setIsDisabled(false) : setIsDisabled(true)
+        isUser && (password === confirmPassword) ? setIsDisabled(false) : setIsDisabled(true)
     }, [userInfo])
 
     function handleOnChange(input, name) {
@@ -42,36 +42,39 @@ export default function SignUp() {
             console.log('err', err)
         })
         setIsLoading(false)
+        props.history.push('/signin')
     }
-  
+
     return (
         <div>
             <div>{isLoading ? <Spinner /> : ''}</div>
             {errMsg ? <ErrorMessage errMsg={errMsg} /> : null}
-            <div className="card">
-                <form className="card-body">
-                    <div className="form-group ">
-                        <div className="row">
-                            <div className="col-6">
-                                <ReusableInputField label="First Name" id="firstName" name="firstName" type="text" placeholder="First Name" onUpdateInput={handleOnChange} />
-                            </div>
-                            <div className="col-6">
-                                <ReusableInputField label="last Name" id="lastName" name="lastName" type="text" placeholder="First Name" onUpdateInput={handleOnChange} />
+            <div>{!isLoading ?
+                <div className="card">
+                    <form className="card-body">
+                        <div className="form-group ">
+                            <div className="row">
+                                <div className="col-6">
+                                    <ReusableInputField label="First Name" id="firstName" name="firstName" type="text" placeholder="First Name" onUpdateInput={handleOnChange} />
+                                </div>
+                                <div className="col-6">
+                                    <ReusableInputField label="last Name" id="lastName" name="lastName" type="text" placeholder="First Name" onUpdateInput={handleOnChange} />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <ReusableInputField label="Email" id="exampleInputEmail1" name="email" type="email" placeholder="Enter email" onUpdateInput={handleOnChange} />
-                    </div>
-                    <div className="form-group">
-                        <ReusableInputField label="Password" type="password" name="password" placeholder="password" onUpdateInput={handleOnChange} />
-                    </div>
-                    <div class="form-group">
-                        <ReusableInputField label="Confirm Password" type="password" name="confirmPassword" placeholder="password" onUpdateInput={handleOnChange} />
-                    </div>
-                    <button type="submit" class="btn btn-primary" disabled={isDisabled} onClick={handleSignUp}>Submit</button>
-                </form>
-            </div>
+                        <div className="form-group">
+                            <ReusableInputField label="Email" id="exampleInputEmail1" name="email" type="email" placeholder="Enter email" onUpdateInput={handleOnChange} />
+                        </div>
+                        <div className="form-group">
+                            <ReusableInputField label="Password" type="password" name="password" placeholder="password" onUpdateInput={handleOnChange} />
+                        </div>
+                        <div class="form-group">
+                            <ReusableInputField label="Confirm Password" type="password" name="confirmPassword" placeholder="password" onUpdateInput={handleOnChange} />
+                        </div>
+                        <button type="submit" class="btn btn-primary" disabled={isDisabled} onClick={handleSignUp}>Submit</button>
+                    </form>
+                </div>
+                : ''}</div>
         </div>
     )
 }
